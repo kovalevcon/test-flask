ccdc3a54accb9f672f40d5c7af8f793b51053a72
Constantine <kovalevcon@mail.ru>
2018-02-05 23:19:48 +0600

    Create app.py
    Create py file

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+app = Flask(__name__)
+
+# Array random string
+array_strings = [
+  "asjdgasdvh1hj23",
+  "asdghjasg12nmbm",
+  "13122312asdwetg",
+  "daih23jkghasdjk"
+]
+
+@app.route("/")
+def index():
+    str = random.choice(array_strings)
+    return render_template('index.html', str=str)
+
+if __name__ == "__main__":
+app.run(host="0.0.0.0")
